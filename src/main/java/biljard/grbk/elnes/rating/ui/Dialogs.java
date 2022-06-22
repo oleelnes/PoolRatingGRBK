52e3586245f18088460182778be42bf17a9b37f7
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-06-22 09:01:30 +0200

    Added two generalized dialog methods to Dialogs.java

--- src/main/java/biljard/grbk/elnes/rating/ui/Dialogs.java
+++ src/main/java/biljard/grbk/elnes/rating/ui/Dialogs.java
@@ -1,4 +1,28 @@
 package biljard.grbk.elnes.rating.ui;
 
+import javafx.scene.control.Alert;
+
 public class Dialogs {
+
+  /**
+   * Method that opens an error alert.
+   *
+   * @param errorText The text of the error alert.
+   */
+  public void errorAlert(String errorText) {
+    Alert alert = new Alert(Alert.AlertType.ERROR, errorText);
+    alert.setTitle("ERROR");
+    alert.showAndWait();
+  }
+
+  /**
+   * Method that opens an information alert.
+   *
+   * @param informationText The text to be displayed.
+   */
+  public void informationAlert(String informationText) {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION, informationText);
+    alert.setTitle("Information");
+    alert.showAndWait();
+  }
 }
