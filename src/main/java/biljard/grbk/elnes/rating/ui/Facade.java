1291bed6650385dd4e188974306f2c1b8d65e6c1
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-06-22 09:02:46 +0200

    Added some of the classes to be held by the Facade.java

--- src/main/java/biljard/grbk/elnes/rating/ui/Facade.java
+++ src/main/java/biljard/grbk/elnes/rating/ui/Facade.java
@@ -1,10 +1,18 @@
 package biljard.grbk.elnes.rating.ui;
 
+import javafx.stage.Stage;
+
 public class Facade {
   private static volatile Facade instance;
+  private final Scenes scenes;
+  private final Dialogs dialogs;
+  private Stage stage;
+
+
 
   private Facade() {
-
+    this.scenes = new Scenes();
+    this.dialogs = new Dialogs();
   }
 
   public static Facade getInstance(){
@@ -14,6 +22,19 @@ public class Facade {
       }
     }
     return instance;
+  }
+
+  public void setStage(Stage stage) {
+    this.stage = stage;
+  }
+
+  public Stage getStage(){
+    try {
+      return stage;
+    } catch(NullPointerException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 
 
