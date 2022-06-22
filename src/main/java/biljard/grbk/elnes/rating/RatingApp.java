78289191e000ffdb3e439797af4db978227e5552
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-06-22 09:03:41 +0200

    RatingApp.java now sets the Stage of the Facade, which will be accessed by Scenes.java.

--- src/main/java/biljard/grbk/elnes/rating/RatingApp.java
+++ src/main/java/biljard/grbk/elnes/rating/RatingApp.java
@@ -1,18 +1,29 @@
 package biljard.grbk.elnes.rating;
 
+import biljard.grbk.elnes.rating.ui.Facade;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RatingApp extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(RatingApp.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
+    Facade.getInstance().setStage(stage);
+
+    //stage.initStyle(StageStyle.UTILITY);
+    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml-files/mainPage.fxml")));
+    Scene scene = new Scene(root);
+    //scene.getStylesheets().add(Objects.requireNonNull(getClass().getClassLoader().getResource("darkmode.css")).toExternalForm());
+    stage.setMinHeight(650);
+    stage.setMinWidth(820);
+    stage.setTitle("Rating");
     stage.setScene(scene);
     stage.show();
   }
